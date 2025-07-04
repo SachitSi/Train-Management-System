3ad63460e0d54ffe7aace508a4ca9dc367b1437b
SachitSi <80474794+SachitSi@users.noreply.github.com>
2025-07-04 00:51:40 -0300

    init

---
+++ TrainManagementSystem/src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,10 @@
+package ticket.booking.entities;
+
+public class Ticket {
+    private String ticket;
+    private String user;
+    private String source;
+    private String destination;
+    private String dateOfTravel;
+    private Train train;
+}
---
+++ TrainManagementSystem/src/main/java/ticket/booking/entities/User.java
@@ -0,0 +1,13 @@
+package ticket.booking.entities;
+
+import java.util.List;
+
+public class User {
+
+    private String name;
+    private String password;
+    private String hashedPassword;
+    private List<Ticket> ticketBooked;
+    private String Uuid;
+
+}
